02e88913629d3ee7f780ee15f2991f2edfdba36c
Carly Clift <carlymclift@gmail.com>
2020-03-29 21:07:13 -0600

    Refute the function isWhite

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -3,9 +3,12 @@ class Unicorn {
     this.name = name;
     this.color = color || "white";
   }
+  isWhite() {
+    if (this.color != "white") {
+      return false
+    }
+  says(string)
+  }
 }
-
-
-
 
 module.exports = Unicorn;
